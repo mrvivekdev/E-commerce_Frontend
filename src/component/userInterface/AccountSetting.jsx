74c717f11915bc07a63c63de8269847e743e8c9c
mrvivekdev <tyou5638@gmail.com>
2024-12-19 14:47:27 +0530

    adding all fildes and make work

--- src/component/userInterface/AccountSetting.jsx
+++ src/component/userInterface/AccountSetting.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState, useContext } from "react";
 import axios from "axios";
+import Cookies from "js-cookie";
 
 import "./AccountSetting.css"
 import TopNavBar from "../homepage/TopNavBar"
@@ -7,15 +8,39 @@ import { ManageContext } from '../../ContextStateManagement';
 
 export default function AccountSetting(){
 
-    const {userData} = useContext(ManageContext);
+    const {userData, setStateCookie, setUserData} = useContext(ManageContext);
 
-    const [email, setEmail] = useState();
-    const [fullname, setFullname] = useState();
-    const [phonenumber, setPhonenumber] = useState();
+    const [address, setAddress] = useState();
+    const [pincodeNumber, setPincodeNumber] = useState();
+    const [city, setCity] = useState();
+    const [street, setStreet] = useState();
+    const [houseNumber, setHouseNumber] = useState();
+    const [landmark, setLandmark] = useState();
     const [updateFullname, setUpdateFullname] = useState();
     const [updatePhonenumber, setUpdatePhonenumber] = useState();
     const [serverData, setServerData] = useState({});
 
+    // Changing delivery information
+    function addressChange(event){
+        setAddress(event.target.value);
+    }
+    function pincodeNumberChange(event){
+        setPincodeNumber(event.target.value);
+    }
+    function cityChange(event){
+        setCity(event.target.value);
+    }
+    function streetChange(event){
+        setStreet(event.target.value);
+    }
+    function houseNumberChange(event){
+        setHouseNumber(event.target.value);
+    }
+    function landmarkChange(event){
+        setLandmark(event.target.value);
+    }
+
+    // Changing basic infop
     function nameChange(event){
         setUpdateFullname(event.target.value);
     }
@@ -30,12 +55,44 @@ export default function AccountSetting(){
                 phonenumber: updatePhonenumber
             })
             setServerData(response.data);
+            cookieCheck();
 
         } catch(error) {
             console.log('Axios error', error);
         }
     }
+
+    async function HandelDeliveryInfoUpdate(){
+        try {
+            const response = await axios.post("/api/user/update", {
+                address: address,
+                pincodeNumber: pincodeNumber,
+                city: city,
+                street: street,
+                houseNumber: houseNumber,
+                landmark: landmark
+            })
+            setServerData(response.data);
+            cookieCheck();
+
+        } catch(error) {
+            console.log('Axios error account update error', error);
+        }
+    }
+
     console.log("serverDataUpdate",serverData);
+
+    if(serverData.cookie && serverData.user){
+        Cookies.set('uid', serverData.cookie);  
+        setUserData(serverData.user)
+    }
+
+    function cookieCheck(){
+        const cookie = Cookies.get('uid');
+        if(cookie){
+            setStateCookie(cookie)
+        }
+    }
 
     return(
         <>
@@ -75,23 +132,67 @@ export default function AccountSetting(){
             <div className="DeliveryInfo">
 
                 <p className="InfoP" >Delivery Address: {"Not Found!"}</p>
-                <input className="InputChange" type="text" placeholder="Add Address"/>
+                <input 
+                    className="InputChange" 
+                    type="text" 
+                    placeholder="Add Address"
+                    onChange={function(event){
+                        addressChange(event)
+                    }}
+                />
 
                 <p className="InfoP" >Address Pincode Number:- {"Not Found!"}</p>
-                <input className="InputChange" type="text" placeholder="Add Pincode"/>
+                <input 
+                    className="InputChange" 
+                    type="text" 
+                    placeholder="Add Pincode"
+                    onChange={function(event){
+                        pincodeNumberChange(event)
+                    }}
+                />
 
                 <p className="InfoP" >City:- {"Not Found!"}</p>
-                <input className="InputChange" type="text" placeholder="City"/>
+                <input 
+                    className="InputChange" 
+                    type="text" 
+                    placeholder="City"
+                    onChange={function(event){
+                        cityChange(event)
+                    }}
+                />
+
                 <p className="InfoP" >Street:- {"Not Found!"}</p>
-                <input className="InputChange" type="text" placeholder="Street"/>
+                <input 
+                    className="InputChange" 
+                    type="text" 
+                    placeholder="Street"
+                    onChange={function(event){
+                        streetChange(event)
+                    }}
+                />
+
                 <p className="InfoP" >House Number:- {"Not Found!"}</p>
-                <input className="InputChange" type="text" placeholder="House Number"/>
+                <input 
+                    className="InputChange" 
+                    type="text" 
+                    placeholder="House Number"
+                    onChange={function(event){
+                        houseNumberChange(event)
+                    }}
+                />
 
                 <p className="InfoP" >Landmark:- {"Not Found!"}</p>
                 <p className="InfoStar SpaceInfo">*This not compalcase to add.</p>
-                <input className="InputChange" type="text" placeholder="Landmark"/>
+                <input 
+                    className="InputChange" 
+                    type="text" 
+                    placeholder="Landmark"
+                    onChange={function(event){
+                        landmarkChange(event)
+                    }}
+                />
 
-                <button className="ChangeBtn" >Submit</button>
+                <button className="ChangeBtn" onClick={HandelDeliveryInfoUpdate} >Submit</button>
             </div>
 
             <h2>Payment Information</h2>
